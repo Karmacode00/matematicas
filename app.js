2e881d7a0795a6ce94c99c2ac0429c748e62bc52
AndreaL <alafertteo@gmail.com>
2018-05-27 23:24:27 -0400

    Inicio ejercicio matematicas

---
+++ app.js
@@ -0,0 +1,27 @@
+window.onload = alert('36325 * (9824 + 777)');
+
+// Variables
+let num1 = 777;
+let num2 = 9824;
+
+
+// Función
+function add() {
+  return num1 + num2;
+}
+
+add(); // Retorna resultado
+
+// Sería una función anidada
+function result () {
+  let num3 = 36325,
+     
+  
+function multiply() {
+    return add * num3;
+  }
+  
+  multiply();
+}
+
+resultadoFinal(); 
